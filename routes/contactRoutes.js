53838947e8b40f3cfdeffe076b0183a07beb52d7
mnesh01 <97187030+mnesh01@users.noreply.github.com>
2025-02-18 11:41:41 +0300

    add CRUD operations

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -6,4 +6,20 @@ router.route("/").get((req, res) => {
     res.status(200).json({message: "Get all contacts"});
 });
 
+router.route("/").post((req, res) => {
+    res.status(200).json({message: "Create contact"});
+});
+
+router.route("/:id").get((req, res) => {
+    res.status(200).json({message: `get contact for ${req.params.id}`});
+});
+
+router.route("/:id").put((req, res) => {
+    res.status(200).json({message: `update contact for ${req.params.id}`});
+});
+
+router.route("/:id").delete((req, res) => {
+    res.status(200).json({message: `delete contact for ${req.params.id}`});
+});
+
 module.exports = router;
