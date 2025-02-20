8a7d21b6ac7afca4bb0c0bfd8b7371803fa26bb5
mnesh01 <97187030+mnesh01@users.noreply.github.com>
2025-02-20 11:47:45 +0300

    setup Db connection

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
+const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
 const app = express();
 
+connectDb();
 const port = process.env.port || 5004;
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
