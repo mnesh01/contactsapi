aa822c1c967377423631ac3fe93c6fbaf479169b
mnesh01 <97187030+mnesh01@users.noreply.github.com>
2025-02-21 14:38:09 +0300

    Add user authentication with JWT token generation

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel")
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken");
 
 //@desc Register a user
 //@route POST /api/users/register
@@ -41,7 +42,29 @@ const registerUser = asyncHandler(async (req, res) =>{
 //@route POST /api/users/register
 //@access public
 const loginUser = asyncHandler(async (req, res) =>{
-    res.json({message: "login user"});
+    const { email, password } = req.body;
+    if (!email || !password) {
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    }
+    const user = await User.findOne({email});
+    //compare pass with hashed
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const accessToken = jwt.sign({
+            user:{
+                username: user.username,
+                email: user.email,
+                id: user.id,
+            },
+        },  process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1m"}
+    );
+        res.status(200).json({ accessToken})
+    }else {
+        res.status(401)
+        throw new Error("Email/Password is not valid")
+
+    }
 });
 
 //@desc current user info
