2cc05ccc6c92de176a7fcf905ec1060367915070
mnesh01 <97187030+mnesh01@users.noreply.github.com>
2025-02-21 18:40:15 +0300

    Associate contacts with users

--- models/contactModel.js
+++ models/contactModel.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose')
 
-const contactSchema = mongoose.Schema({
+const contactSchema = mongoose.Schema(
+    {
+        user_id :{
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: "user"
+        },
     name : {
         type: String,
         required: [true, "Add the contact name"],
